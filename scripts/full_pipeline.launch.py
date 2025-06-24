cf325be96f5fca84d49af16c0f1057847953520a
Monisha RK <monisha.rk2415@gmail.com>
2025-06-24 21:06:40 +0200

    Add files via upload

---
+++ scripts/full_pipeline.launch.py
@@ -0,0 +1,33 @@
+from launch import LaunchDescription
+from launch.actions import TimerAction
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        # Start stereo_depth_node immediately
+        Node(
+            package='perception_pipeline',
+            executable='stereo_depth_node',
+            name='stereo_depth_node',
+            output='screen'
+        ),
+        # Start object_fusion_warning_node immediately
+        Node(
+            package='perception_pipeline',
+            executable='object_fusion_warning_node',
+            name='object_fusion_warning_node',
+            output='screen'
+        ),
+        # Delay the stereo_image_publisher node by 2 seconds
+        TimerAction(
+            period=5.0,
+            actions=[
+                Node(
+                    package='perception_pipeline',
+                    executable='stereo_image_publisher',
+                    name='stereo_image_publisher_node',
+                    output='screen'
+                )
+            ]
+        )
+    ])
