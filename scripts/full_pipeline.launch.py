62843d3ac4b1847b69fb6b9d2b41f8680fa46eef
Monisha RK <monisha.rk2415@gmail.com>
2025-06-24 03:27:07 +0200

    Add files via upload

---
+++ scripts/full_pipeline.launch.py
@@ -0,0 +1,24 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='perception_pipeline',
+            executable='stereo_image_publisher',
+            name='stereo_image_publisher_node',
+            output='screen'
+        ),
+        Node(
+            package='perception_pipeline',
+            executable='stereo_depth_node',
+            name='stereo_depth_node',
+            output='screen'
+        ),
+        Node(
+            package='perception_pipeline',
+            executable='object_fusion_warning_node',
+            name='object_fusion_warning_node',
+            output='screen'
+        )
+    ])
