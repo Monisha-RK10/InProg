c4022ae8473a0b67e5b702c4508fd4e63060acba
Monisha RK <monisha.rk2415@gmail.com>
2025-06-24 18:21:21 +0200

    Update stereo_depth_node.py

--- scripts/stereo_depth_node.py
+++ scripts/stereo_depth_node.py
@@ -22,12 +22,12 @@
 
 import rclpy
 from rclpy.node import Node
-from sensor_msgs.msg import Image, PointCloud2
+from sensor_msgs.msg import Image, PointCloud2                                            # ROS topics
 from std_msgs.msg import Header
 from cv_bridge import CvBridge
 import numpy as np
 import cv2
-import sensor_msgs_py.point_cloud2 as pc2
+import sensor_msgs_py.point_cloud2 as pc2                                                 # Helper functions to manipulate PointCloud2 data in Python.
 
 class StereoDepthNode(Node):
     def __init__(self):
